5deb2febf9046fc421dcad74d6b251273da56efd
Yi-Hua Liang <yihua1218@gmail.com>
2017-04-13 14:50:48 +0800

    Added .xim_config.js sample file.

---
+++ .xim_config.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017
+ * Qblinks Incorporated ("Qblinks").
+ * All rights reserved.
+ *
+ * The information contained herein is confidential and proprietary to
+ * Qblinks. Use of this information by anyone other than authorized employees
+ * of Qblinks is granted only under a written non-disclosure agreement,
+ * expressly prescribing the scope and manner of such use.
+ */
+
+module.exports = {
+  quantum_token: '46711f8b0b67c56187a45096dbb44cf6ad79c86595e11a68860bf470f8d99ad8',
+  device_id: 'MY_DEVICE_ID',
+};
